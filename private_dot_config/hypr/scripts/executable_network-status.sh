48aa907b294da58fc70a34c4a5673e2207620c0f
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-06-27 15:00:31 +0200

    Update .config/hypr/scripts/network-status.sh

--- private_dot_config/hypr/scripts/executable_network-status.sh
+++ private_dot_config/hypr/scripts/executable_network-status.sh
@@ -1,5 +1,10 @@
 
 #!/usr/bin/zsh
 
-ip route | grep default | awk '{print $5}'
-
+ADAPTER=$(ip route | grep default | awk '{print $5}')
+if [[ $ADAPTER == w* ]]; then
+	SSID=$(echo 'station wlan0 show' | iwctl | awk '/Connected network/ {for (i=1; i<=NF; i++) if ($i == "network") {print $(i+1); for (j=i+2; j<=NF; j++) printf " %s", $j; print ""; break}}')
+	echo "$ADAPTER:" $SSID
+else
+	echo $ADAPTER
+fi
