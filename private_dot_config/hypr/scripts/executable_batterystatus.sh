a385f3168e8335712bd89e8875a9aaca1883eade
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-08-05 18:35:04 +0200

    Update .config/hypr/scripts/batterystatus.sh

--- private_dot_config/hypr/scripts/executable_batterystatus.sh
+++ private_dot_config/hypr/scripts/executable_batterystatus.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/zsh
+BATTERY_DIR=$(ls -d /sys/class/power_supply/BAT* | head -n 1)
 
-echo $(acpi -b | sed -E 's/Battery [0-9]+: ([^,]+), ([^,]+), (.*) until charged/\2, \1, Full in \3/')
+CURRENT_PERCENT=$(cat "${BATTERY_DIR}/capacity")
+STATUS=$(cat ${BATTERY_DIR}/status)
+
+echo ${CURRENT_PERCENT}%, ${STATUS}
