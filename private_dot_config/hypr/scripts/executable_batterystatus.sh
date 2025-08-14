488e4e6af1d221d0efccf57bb370360c8e7febbb
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-08-15 01:21:51 +0200

    Added lazy.nvim

--- private_dot_config/hypr/scripts/executable_batterystatus.sh
+++ private_dot_config/hypr/scripts/executable_batterystatus.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/zsh
-BATTERY_DIR=$(ls -d /sys/class/power_supply/BAT* | head -n 1)
+for BATTERY_DIR in /sys/class/power_supply/BAT*; do
+	BATTERY_NAME=$(basename${BATTERY_DIR))
+	PERCENT=$(cat "${BATTERY_DIR}/capacity")
+	STATUS=$(cat "${BATTERY_DIR}/status")
 
-CURRENT_PERCENT=$(cat "${BATTERY_DIR}/capacity")
-STATUS=$(cat ${BATTERY_DIR}/status)
-
-echo ${CURRENT_PERCENT}%, ${STATUS}
+	echo "${BATTERY_NAME}: ${PERCENT}%, ${STATUS}\n"
+done
---
+++ private_dot_config/nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- Change the style if you want, `night` is the default
+    style = "storm",
+    -- To enable transparent background
+    transparent = true,
+  },
+}
