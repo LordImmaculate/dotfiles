32ecd4291348b1369c47fe5f3edaf15f770dbfa9
LordImmaculate <101836390+LordImmaculate@users.noreply.github.com>
2025-08-27 12:28:25 +0200

    Update neovim config's

--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -1,18 +1,25 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- This setup tries prettierd first, then falls back to prettier
-				javascript = { "prettierd", "prettier" },
-				c = { "cland" },
-			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_fallback = true, -- Use lsp_fallback = true
-			},
-		})
-	end,
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					-- This setup tries prettierd first, then falls back to prettier
+					javascript = { "prettierd", "prettier" },
+					c = { "cland" },
+					hyprlang = { "hyprls" },
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_fallback = true, -- Use lsp_fallback = true
+				},
+			})
+		end,
+	},
+	{
+
+		"zapling/mason-conform.nvim",
+	},
 }
